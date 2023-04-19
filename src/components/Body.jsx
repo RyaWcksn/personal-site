77acc4e349e526327b80e7b84dec4b5f8b715e17
RyaWcksn <pram.aryawcksn@protonmail.ch>
2023-04-19 16:00:22 +0700

    update

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
-import profile from '../assets/profile.jpeg'
-import '../assets/styles/body.css'
+import '../assets/styles/body.css';
+import { Box, Grid} from '@mui/material';
 
 function Body() {
     return(
-        <div className='body-container'>
-          <div className='body-picture'>
-            <img src={profile} alt='Profile' className='profile-picture'/>
-          </div>
-          <div className='body-profile'>
-            <div className='body-heading'>
-            <h1>
-              Hello There! <br />
-              My name is Pramudya Arya Wicaksana
-            </h1>
-            </div>
-            <div className='body-subheading'>
-            <h2>
-              Software Engineer who passionate in Cloud Engineering and Fintech!
-            </h2>
-            </div>
-          </div>
-        </div>
-    )
+        <Box className='container'>
+          <Grid container spacing={1}>
+            <Grid item xs={4} md={8}>
+              <h1>
+                Hello! Greeting
+              </h1>
+              <h2>
+                I'm Pramudya Arya Wicaksana
+              </h2>
+            </Grid>
+          </Grid>
+        </Box>
+    );
 }
 
-export default Body
+export default Body;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import '../assets/styles/header.css'
+import React from 'react';
+import '../assets/styles/header.css';
+import ComputerIcon from '@mui/icons-material/Computer';
+import { AppBar, Grid, Link, Toolbar, useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
 
 function Header() {
+    const theme = useTheme();
+    const isPhoneScreen = useMediaQuery(theme.breakpoints.down('sm'));
+    const appBarHeight = isPhoneScreen ? '80px' : '64px';
 	return (
-		<div className='row header'>
-			<div className='col header-home'>
-				<a href='/'>Pramudya Arya Wicaksana</a>
-			</div>
-			<div className='col header-info'>
-				<div className='col header-blog'>
-					<a href='https://blog.yuuriya.tech'>Blog</a>
-				</div>
-			</div>
-		</div>
-	)
+            <AppBar position='static' sx={{background: '#b2b2b2', justifyContent: 'center', padding: '20px', height: appBarHeight}}>
+              <Toolbar sx={{ justifyContent: 'center' }}>
+                <Grid container columnSpacing={3} sx={{display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+                  <Grid item>
+                    <Link href='/' style={{ textDecoration: 'none' }} sx={{ fontSize: '2rem', color: '#fff' }}><ComputerIcon sx={{ fontSize: '3rem', color: '#fff' }}/></Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href='https://blog.yuuriya.tech' style={{ textDecoration: 'none' }} sx={{ fontSize: '2rem', color: '#fff' }}>Blog</Link>
+                  </Grid>
+                </Grid>
+              </Toolbar>
+            </AppBar>
+	);
 
 }
 
-export default Header
+export default Header;
